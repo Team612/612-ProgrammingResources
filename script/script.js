1cc85c2be243aec2f96ca3693f0c093604b7cb94
Calvin Godfrey <calvinbbcard@gmail.com>
2017-01-04 19:46:53 -0500

    Moved the 612-resources repo here, added link to encrypted game manual

---
+++ script/script.js
@@ -0,0 +1,70 @@
+$(document).ready(function(){
+var count = 0;
+document.addEventListener('keydown', function(event){
+  switch(event.keyCode){
+    case 38:
+      if(count==0||count==1)count++;
+      else count=0;
+      break;
+    case 40:
+      if(count==2||count==3)count++;
+      else count=0;
+      break;
+    case 37:
+      if(count==4||count==6)count++;
+      else count=0;
+      break;
+    case 39:
+      if(count==5||count==7)count++;
+      else count=0;
+      break;
+    case 66:
+      if(count==8)count++;
+      else count=0;
+      break;
+    case 65:
+      if(count==9)count++;
+      else count = 0;
+      break;
+    case 13:
+      if(count==10){
+        $("body").css("font-family", "Times New Roman");
+        $("body").css("background", 'url("../img/logo.png")');
+        $("body").css("background-size", "50% 50%");
+        $(".navbar").css("background", 'url("../img/logo.png")');
+        $(".navbar").css("background-size", "50% 50%");
+        $("#logo").css("background-color", "white");
+        $("#logo").css("background", 'url9"../img/logo.png")');
+        $("#logo").css("background-size", "25% 25%");
+        $("#logo").css("width", "20%");
+        $("#logo").css("padding", "0 0 0 0");
+        $("*").css("color", "black");
+      }
+      else count = 0;
+      break;
+    default:
+      count = 0;
+  }
+});
+
+var links=[".genlink", ".robotlink", ".visionlink", ".weblink", ".telelink", ".homelink"];
+var content=[".general", ".robot", ".vision", ".website", ".telemetry", ".home"];
+
+var changeLink = function(toActive, body){
+  for(var i=0;i<links.length;i++){
+    $(links[i]).removeClass("active");
+  };
+  $(toActive).addClass('active');
+  for(var i=0;i<links.length;i++){
+    $(content[i]).hide();
+  }
+  $(body).show();
+}
+
+$(".homelink").click(function(){changeLink(".homelink", ".home");});
+$(".genlink").click(function(){changeLink(".genlink", ".general");});
+$(".robotlink").click(function(){changeLink(".robotlink", ".robot");});
+$(".visionlink").click(function(){changeLink(".visionlink", ".vision");});
+$(".weblink").click(function(){changeLink(".weblink", ".website");});
+$(".telelink").click(function(){changeLink(".telelink", ".telemetry");});
+});
