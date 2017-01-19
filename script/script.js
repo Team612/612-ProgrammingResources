33994f4e5b006c7cb6dfe66c97df4644c93ab3fb
Calvin Godfrey <calvinbbcard@gmail.com>
2017-01-19 10:01:13 -0500

    Added basic responsiveness. Basic animation when you change the active tab. Will break if you rapidly change tabs, that's the next step

--- script/script.js
+++ script/script.js
@@ -1,108 +1,109 @@
 $(document).ready(function(){
-var count = 0;
-document.addEventListener('keydown', function(event){ //Konami Code
-  switch(event.keyCode){
-    case 38:
-      if(count==0||count==1)count++; //Up arrow
-      else count=0;
-      break;
-    case 40:
-      if(count==2||count==3)count++; //Down arrow
-      else count=0;
-      break;
-    case 37:
-      if(count==4||count==6)count++; //Left Arrow
-      else count=0;
-      break;
-    case 39:
-      if(count==5||count==7)count++; //Right arrow
-      else count=0;
-      break;
-    case 66:
-      if(count==8)count++; //B
-      else count=0;
-      break;
-    case 65:
-      if(count==9)count++; //A
-      else count = 0;
-      break;
-    case 13:
-      if(count==10){ //Enter
-        $("body").css("font-family", "Times New Roman");
-        $("body").css("background", 'url("../img/logo.png")');
-        $("body").css("background-size", "50% 50%");
-        $(".navbar").css("background", 'url("../img/logo.png")');
-        $(".navbar").css("background-size", "50% 50%");
-        $("#logo").css("background-color", "white");
-        $("#logo").css("background", 'url9"../img/logo.png")');
-        $("#logo").css("background-size", "25% 25%");
-        $("#logo").css("width", "20%");
-        $("#logo").css("padding", "0 0 0 0");
-        $("*").css("color", "black");
+  var count = 0;
+  document.addEventListener('keydown', function(event){ //Konami Code
+    switch(event.keyCode){
+      case 38:
+        if(count==0||count==1)count++; //Up arrow
+        else count=0;
+        break;
+      case 40:
+        if(count==2||count==3)count++; //Down arrow
+        else count=0;
+        break;
+      case 37:
+        if(count==4||count==6)count++; //Left Arrow
+        else count=0;
+        break;
+      case 39:
+        if(count==5||count==7)count++; //Right arrow
+        else count=0;
+        break;
+      case 66:
+        if(count==8)count++; //B
+        else count=0;
+        break;
+      case 65:
+        if(count==9)count++; //A
+        else count = 0;
+        break;
+      case 13:
+        if(count==10){ //Enter
+          $("body").css("font-family", "Times New Roman");
+          $("body").css("background", 'url("../img/logo.png")');
+          $("body").css("background-size", "50% 50%");
+          $(".navbar").css("background", 'url("../img/logo.png")');
+          $(".navbar").css("background-size", "50% 50%");
+          $("#logo").css("background-color", "white");
+          $("#logo").css("background", 'url9"../img/logo.png")');
+          $("#logo").css("background-size", "25% 25%");
+          $("#logo").css("width", "20%");
+          $("#logo").css("padding", "0 0 0 0");
+          $("*").css("color", "black");
+        }
+        else count = 0;
+        break;
+      default:
+        count = 0;
+    }
+  });
+
+  var general = { //Object for each of the main content blocks
+    link: "#genlink",
+    body: "#general",
+    isActive: false
+  };
+
+  var robot = {
+    link: "#robotlink",
+    body: "#robot",
+    isActive: false
+  };
+
+  var vision = {
+    link: "#visionlink",
+    body: "#vision",
+    isActive: false
+  };
+
+  var website = {
+    link: "#weblink",
+    body: "#website",
+    isActive: false
+  };
+
+  var telemetry = {
+    link: "#telelink",
+    body: "#telemetry",
+    isActive: false
+  };
+
+  var home = {
+    link: "#homelink",
+    body: "#home",
+    isActive: true
+  };
+
+  var content=[home, general, robot, vision, website, telemetry];
+
+  var changeLink = function(obj){ //Changes the content block that is currently displayed
+    for(var i=0;i<content.length;i++){
+      if(content[i].isActive){
+        content[i].isActive = false;
+        $(content[i].link).removeClass("active"); //Changes background of the navbar at the top
+        $(obj.link).addClass("active");
+        obj.isActive = true;
+        $(content[i].body).slideUp(500, function(){ //Hide the actual content
+          $(obj.body).slideDown(500); //And then display the new
+        });
+        break;
       }
-      else count = 0;
-      break;
-    default:
-      count = 0;
-  }
-});
-
-var general = { //Object for each of the main content blocks
-  link: "#genlink",
-  body: "#general",
-  isActive: false
-};
-
-var robot = {
-  link: "#robotlink",
-  body: "#robot",
-  isActive: false
-};
-
-var vision = {
-  link: "#visionlink",
-  body: "#vision",
-  isActive: false
-};
-
-var website = {
-  link: "#weblink",
-  body: "#website",
-  isActive: false
-};
-
-var telemetry = {
-  link: "#telelink",
-  body: "#telemetry",
-  isActive: false
-};
-
-var home = {
-  link: "#homelink",
-  body: "#home",
-  isActive: true
-};
-
-var content=[home, general, robot, vision, website, telemetry];
-
-var changeLink = function(obj){ //Changes the content block that is currently displayed
-  for(var i=0;i<content.length;i++){
-    if(content[i].isActive){
-      content[i].isActive = false;
-      $(content[i].link).removeClass("active"); //Changes background of the navbar at the top
-      $(obj.link).addClass("active");
-      obj.isActive = true;
-      $(content[i].body).hide(); //Hide the actual content
-      $(obj.body).show(); //And then display the new
-      break;
     }
   }
-}
 
-$("#homelink").click(function(){changeLink(home);});
-$("#genlink").click(function(){changeLink(general);});
-$("#robotlink").click(function(){changeLink(robot);});
-$("#visionlink").click(function(){changeLink(vision);});
-$("#weblink").click(function(){changeLink(website);});
-$("#telelink").click(function(){changeLink(telemetry);});
+  $("#homelink").click(function(){changeLink(home);});
+  $("#genlink").click(function(){changeLink(general);});
+  $("#robotlink").click(function(){changeLink(robot);});
+  $("#visionlink").click(function(){changeLink(vision);});
+  $("#weblink").click(function(){changeLink(website);});
+  $("#telelink").click(function(){changeLink(telemetry);});
 });
